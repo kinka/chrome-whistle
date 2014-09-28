d1cf214d12e812477aef5163b223f75371733ccb
Kinka Huang <kinkabrain@gmail.com>
2014-09-28 23:54:06 +0800

    redirect url, singlton

--- background.js
+++ background.js
@@ -1,33 +1,22 @@
-/*chrome.browserAction.onClicked.addListener(function(tab) {
-	console.log(tab);
-});
-
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://2think.sinaapp.com/pt", true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    // JSON解析器不会执行攻击者设计的脚本.
-    var resp = JSON.parse(xhr.responseText);
-    console.log(resp)
-  }
-}
-xhr.send();
-*/
 var wr = chrome.webRequest;
+var maps = {};
 var cb = function(details) {
 	var url = details.url;
 	var dstUrl = "http://127.0.0.1/static/";
 	var matched = "";
-	var srcUrl = "2think.sinaapp.com".replace(".", "\.") + "/static/(.*)".replace("/", "\/");
-	if (matched = url.match(new RegExp(srcUrl, "i"))) {
-		console.log(matched[1]);
-		dstUrl += matched[1];
-		return {redirectUrl: dstUrl};
-	}
+	for (var online in maps) {
+		matched = url.match(new RegExp(online+"(.*)", "i"));
+// 		console.log(url, matched, maps[online], online);
+		if (!matched) continue;
+		
+		console.log(maps[online]+'/' + matched[1])
+		return {redirectUrl: maps[online]+'/' + matched[1]};
+	};
 }
 var filter = {urls: ["<all_urls>"]};
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-	console.log(request.doBlock)
+	console.log(request);
+	maps[request.online] = request.local;
 	if (request.doBlock)	
 		chrome.webRequest.onBeforeRequest.addListener(cb, filter, ["blocking"]);
 	else
--- popup.js
+++ popup.js
@@ -1,14 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
-	var switcher = document.querySelector('#switcher')
+	var $ = function(selector) {return document.querySelector(selector);},
+		$$ = function(selector) {return document.querySelectorAll(selector);};
+	
+	var switcher = $('.switcher'),
+		online = $('.online'),
+		local = $('.local');
 	switcher.checked = localStorage['doBlock'] ? true:false;
 	switcher.addEventListener('change', function() {
 		console.log(this.checked, localStorage['doBlock'])
-		var _doBlock = this.checked;
-		chrome.extension.sendRequest({doBlock: this.checked},
+
+		sendConfig({doBlock: switcher.checked, online: online.value, local: local.value});
+	});
+	
+	
+	online.oninput = onChange.bind(online);
+	local.oninput = onChange.bind(local);
+	online.value = localStorage['online'] || "www.logger.im/static/";
+	local.value = localStorage['local'] || "http://127.0.0.1:888";
+
+	function onChange() {
+		if (switcher.checked)
+			switcher.checked = false;
+	}
+	
+	sendConfig({doBlock: switcher.checked, online: online.value, local: local.value});
+	function sendConfig(config) {
+		chrome.extension.sendRequest(config,
 			function(response) {
-				localStorage['doBlock'] = _doBlock;
+				localStorage['doBlock'] = config.doBlock;
+				localStorage['online'] = config.online;
+				localStorage['local'] = config.local;
 				console.log(response);
 				chrome.tabs.reload();
-			});
-	});
+		});
+	}
 });
