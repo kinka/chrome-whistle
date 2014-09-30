60801765af2f867ef4e23872bb5a42770c2fc0ce
Kinka <kinkabrain@gmail.com>
2014-09-30 17:39:38 +0800

    multi rules config

--- background.js
+++ background.js
@@ -1,34 +1,42 @@
 var wr = chrome.webRequest;
 var maps = {};
 var cb = function(details) {
-	var url = details.url;
-	var dstUrl = "http://127.0.0.1/static/";
-	var matched = "";
-	for (var online in maps) {
-		var pattern = new RegExp(online, "i");
-		matched = url.match(pattern);
-// 		console.log(url, matched, maps[online], online);
-		if (!matched) continue;
-		
-		console.log(url.replace(pattern, maps[online]))
-		return {redirectUrl: url.replace(pattern, maps[online])};
-	};
+    var url = details.url;
+    var dstUrl = "";
+    var matched = "";
+    for (var id in maps) {
+		var online = maps[id].online,
+			local = maps[id].local;
+        var pattern = new RegExp(online, "i");
+        matched = url.match(pattern);
+//         		console.log(url, matched, local, online);
+        if (!matched)
+            continue;
+
+        // 		console.log(url.replace(pattern, local))
+		dstUrl = url.replace(pattern, local).replace('https://', 'http://');
+        return {redirectUrl: dstUrl};
+    }
 }
-var filter = {urls:[]}; // {urls: ["<all_urls>"]};
+var filter = {urls: []}; // {urls: ["<all_urls>"]};
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-	console.log(request);
-	maps = {};
-	maps[request.online] = request.local;
-	console.log(maps)
-	var matched = request.online.match(new RegExp("(?:http://|^)(.*?)/","i"));
-	if (matched && matched[1])
-		filter.urls = ["*://" + matched[1]+"/*"];
-	else
-		return sendResponse("failed " + matched);
-	console.log(filter.urls)
-	if (request.doBlock)
-		chrome.webRequest.onBeforeRequest.addListener(cb, filter, ["blocking"]);
-	else
-		chrome.webRequest.onBeforeRequest.removeListener(cb);
-	sendResponse();
-})
+    console.log(request);
+    maps[request.id] = {online: request.online, local: request.local};
+    console.log(maps)
+	
+	// update filter
+	filter.urls = [];
+	for (var id in maps) {
+		var matched = maps[id].online.match(new RegExp("(?:http://|^)(.*?)/", "i"));
+		if (matched && matched[1])
+			filter.urls.push("*://" + matched[1] + "/*");
+	}
+
+    console.log('filter', filter.urls)
+    if (request.doBlock)
+        chrome.webRequest.onBeforeRequest.addListener(cb, filter, ["blocking"]);
+    else
+        chrome.webRequest.onBeforeRequest.removeListener(cb);
+	
+    sendResponse('response from background.js');
+});
--- popup.js
+++ popup.js
@@ -1,39 +1,90 @@
 document.addEventListener('DOMContentLoaded', function () {
-	var $ = function(selector) {return document.querySelector(selector);},
-		$$ = function(selector) {return document.querySelectorAll(selector);};
+	var $ = function(selector, parent) {return (parent || document).querySelector(selector);},
+		$$ = function(selector, parent) {return (parent || document).querySelectorAll(selector);};
 	
 	$('#launch_app').onclick = function() {
-		chrome.management.launchApp("oaepcbaobpinkjikdjkignjdpdgpapff");
+		chrome.management.getAll(function(data) {
+			var id = "";
+			var whistleProxy = data.filter(function(e) {
+				return e.name.toLowerCase().indexOf("whistle-proxy") != -1;
+			});
+			if (whistleProxy && whistleProxy[0]) {
+				whistleProxy = whistleProxy[0];
+				id = whistleProxy.id;
+			}
+			
+			try {
+				chrome.management.launchApp(id);
+			} catch (e) {
+				alert('LaunchApp Failed...');
+			}
+		});
+		
 	}
 	
-	var switcher = $('.switcher'),
-		online = $('.online'),
-		local = $('.local');
-	switcher.checked = localStorage['doBlock'] ? true:false;
-	switcher.addEventListener('change', function() {
-		sendConfig({doBlock: switcher.checked, online: online.value, local: local.value});
-	});
+	if (!localStorage['rules'])
+		localStorage['rules'] = JSON.stringify({});
+	var rulesConfig = JSON.parse(localStorage['rules']) || {}; 
 	
-	
-	online.oninput = onChange.bind(online);
-	local.oninput = onChange.bind(local);
-	online.value = localStorage['online'] || "www.logger.im/static/";
-	local.value = localStorage['local'] || "http://127.0.0.1:888";
-
-	function onChange() {
-		if (switcher.checked)
-			switcher.checked = false;
-	}
-	
-	sendConfig({doBlock: switcher.checked, online: online.value, local: local.value});
+// 	sendConfig({doBlock: switcher.checked, online: online.value, local: local.value});
 	function sendConfig(config) {
+		if (!config.id) {
+			console.warn('no rule id');
+			return;
+		}
+		var rule = rulesConfig[config.id] || {};
+		rule['doBlock'] = config.doBlock;
+		rule['online'] = config.online;
+		rule['local'] = config.local;
+		rulesConfig[config.id] = rule;
+		localStorage['rules'] = JSON.stringify(rulesConfig);
+		
 		chrome.extension.sendRequest(config,
 			function(response) {
-				localStorage['doBlock'] = config.doBlock;
-				localStorage['online'] = config.online;
-				localStorage['local'] = config.local;
 				console.log("response", response);
 				chrome.tabs.reload();
 		});
 	}
+	
+	// rules operation
+	var rulesForm = $('#rules_form');
+	var ruleTpl = $("#rule0").innerHTML;
+	
+	for (var i=0; i<5; i++) {
+		var rule = document.createElement("legend");
+		rule.innerHTML = ruleTpl;
+		rule.id = "rule" + (i+1);
+		rulesForm.appendChild(rule);
+	}
+	
+	var legends = [].splice.call($$("legend", rulesForm), 0);
+	legends.forEach(function(rule) {
+		var switcher = $('.switcher', rule),
+			online = $('.online', rule),
+			local = $('.local', rule);
+		switcher.checked = rulesConfig[rule.id]['doBlock'] ? true:false;
+		switcher.onchange = onChange.bind(switcher);
+		
+		online.oninput = onChange.bind(online);
+		local.oninput = onChange.bind(local);
+		online.value = rulesConfig[rule.id]['online'] || "";
+		local.value = rulesConfig[rule.id]['local'] || "";
+		function onChange(e) {
+			if (e.target == local || e.target == online) {
+				if (switcher.checked)
+					switcher.checked = false;
+				return;
+			}
+			// for switcher
+			if (switcher.checked && (!online.value || !local.value))
+				return;
+				
+			sendConfig({
+				id: rule.id, 
+				doBlock: switcher.checked, online: online.value, local: local.value
+			});
+		}
+		
+	});
+	
 });
